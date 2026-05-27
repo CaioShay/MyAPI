c8820b76c01e92fda504ac4c0027553195468e3e
Caio_AKA_MataGato <cssb1@aluno.ifal.br>
2026-05-26 21:43:34 -0300

    Entendendo..

--- api.js
+++ api.js
@@ -1,17 +1,27 @@
+require('dotenv').config();
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const SECRET = process.env.JWT_SECRET;
+
+// verifica se o secret existe
+if (!SECRET) {
+    console.log('JWT_SECRET não encontrado no .env');
+    process.exit(1);
+}else {console.log(SECRET)}
 
 // rota de login
 app.post('/login', (req, res) => {
 
     const { user, password } = req.body;
 
-    // exemplo simples
+    // validação simples
     if (user !== 'caio' || password !== '123') {
         return res.status(401).json({
             error: 'Usuário ou senha inválidos'
@@ -23,7 +33,7 @@ app.post('/login', (req, res) => {
         {
             user: user
         },
-        process.env.JWT_SECRET,
+        SECRET,
         {
             expiresIn: '1h'
         }
@@ -39,27 +49,30 @@ function auth(req, res, next) {
 
     const authHeader = req.headers.authorization;
 
+    // verifica se enviou o token
     if (!authHeader) {
         return res.status(401).json({
             error: 'Token não enviado'
         });
     }
 
-    // Bearer TOKEN
+    // pega o token depois do Bearer
     const token = authHeader.split(' ')[1];
 
+    // verifica token
     try {
 
         const decoded = jwt.verify(
             token,
-            process.env.JWT_SECRET
+            SECRET
         );
 
+        // salva dados do usuário
         req.user = decoded;
 
         next();
 
-    } catch {
+    } catch (err) {
 
         return res.status(401).json({
             error: 'Token inválido'
@@ -79,8 +92,7 @@ app.post('/multiply', auth, (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-
+// inicia servidor
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor rodando na porta ${PORT}!`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
